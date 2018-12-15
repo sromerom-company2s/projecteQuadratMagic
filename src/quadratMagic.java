4dcc1bcbe13cc76193eb49f031b434256dac7f2f
Samuel Romero <sromerom@esliceu.net>
2018-12-15 20:23:03 +0100

    Canvi del nom de la funcio

--- src/quadratMagic.java
+++ src/quadratMagic.java
@@ -1,7 +1,7 @@
 public class quadratMagic {
     public static void main(String[] args) {
         QuadratMagic exemple = new QuadratMagic(5);
-        exemple.imprimeix();
+        exemple.print();
     }
 }
 
@@ -20,7 +20,7 @@ class QuadratMagic {
         }
     }
 
-    void imprimeix() {
+    void print () {
         for (int i = 0; i < dades.length; i++) {
             for (int j = 0; j < dades[i].length; j++) {
                 System.out.printf("%4d", dades[i][j]);
