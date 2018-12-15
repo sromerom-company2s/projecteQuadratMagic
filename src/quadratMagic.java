abe01e31a79ece7f45baedac2756de684dc1c665
Samuel Romero <sromerom@esliceu.net>
2018-12-15 18:36:16 +0100

    Primer estat

---
+++ src/quadratMagic.java
@@ -0,0 +1,35 @@
+public class quadratMagic {
+    public static void main(String[] args) {
+        QuadratMagic exemple = new QuadratMagic(5);
+        exemple.imprimeix();
+    }
+}
+
+class QuadratMagic {
+    int [][] dades;
+    QuadratMagic(int dimensions) {
+        dades = new int [dimensions][dimensions];
+        for (int a = 0; a < dimensions; a++) {
+            dades [0][a] = 1;
+            dades [a][0] = 1;
+        }
+        for (int i = 1; i < dimensions; i++) {
+            for (int j = 1; j < dimensions; j++) {
+                dades [j][i] = dades [j][i - 1] + dades [j - 1][i];
+            }
+        }
+    }
+
+    void imprimeix() {
+        for (int i = 0; i < dades.length; i++) {
+            for (int j = 0; j < dades[i].length; j++) {
+                System.out.printf("%7d", dades[i][j]);
+
+
+            }
+            System.out.println();
+        }
+    }
+
+
+}
